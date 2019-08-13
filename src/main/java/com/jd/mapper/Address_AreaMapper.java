469b2dbb6beed99129444c7a82ec68b7382479d3
Dai Ning <614464386@qq.com>
2019-08-13 20:42:57 +0800

    注释了Address_AreaMapper代码

--- src/main/java/com/jd/mapper/Address_AreaMapper.java
+++ src/main/java/com/jd/mapper/Address_AreaMapper.java
@@ -3,15 +3,15 @@ package com.jd.mapper;
 import com.jd.enity.Address_Area;
 
 public interface Address_AreaMapper {
-    int deleteByPrimaryKey(Integer a_areaid);
-
-    int insert(Address_Area record);
-
-    int insertSelective(Address_Area record);
-
-    Address_Area selectByPrimaryKey(Integer a_areaid);
-
-    int updateByPrimaryKeySelective(Address_Area record);
-
-    int updateByPrimaryKey(Address_Area record);
+//    int deleteByPrimaryKey(Integer a_areaid);
+//
+//    int insert(Address_Area record);
+//
+//    int insertSelective(Address_Area record);
+//
+//    Address_Area selectByPrimaryKey(Integer a_areaid);
+//
+//    int updateByPrimaryKeySelective(Address_Area record);
+//
+//    int updateByPrimaryKey(Address_Area record);
 }
